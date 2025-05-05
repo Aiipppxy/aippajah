ba24b5a6da55b1af7113dea4c58de71f0ec01bad
Aiipppxy <231230051@unmuhpnk.ac.id>
2025-05-05 12:22:27 +0700

    Admin Proyek

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactController;
-
+use App\Http\Controllers\AdminController;
 Route::get('/', [ProductController::class, 'index'])->name('home');;
 // Route untuk menampilkan daftar produk
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
@@ -12,3 +12,5 @@ Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/contact', [ContactController::class, 'index']);
 Route::post('/contact', [ContactController::class, 'store']);
+Route::get('/admin', [AdminController::class, 'create']);
+Route::post('/admin', [AdminController::class, 'store']);
