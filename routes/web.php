136524c200b5b9e590940b3fc14e3e81a2e75c17
Aiipppxy <231230051@unmuhpnk.ac.id>
2025-05-19 12:08:28 +0700

    Buat halaman dalam blade

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminController;
 Route::get('/', [ProductController::class, 'index'])->name('home');;
@@ -14,3 +13,18 @@ Route::get('/contact', [ContactController::class, 'index']);
 Route::post('/contact', [ContactController::class, 'store']);
 Route::get('/admin', [AdminController::class, 'create']);
 Route::post('/admin', [AdminController::class, 'store']);
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
